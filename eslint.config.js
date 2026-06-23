560d917b0b3074ead43da0fcfa6d97de17c80457
dparavandi <dparavandi@wiley.com>
2026-06-23 16:04:45 +0300

    Wire ds/no-hardcoded-colors rule into eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,19 +4,27 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import dsRules from './eslint/no-hardcoded-colors.cjs'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+    globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-    },
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+                js.configs.recommended,
+                tseslint.configs.recommended,
+                reactHooks.configs.flat.recommended,
+                reactRefresh.configs.vite,
+              ],
+        languageOptions: {
+                globals: globals.browser,
+        },
   },
-])
+  {
+        files: ['src/**/*.{ts,tsx}'],
+        plugins: { ds: dsRules },
+        rules: {
+                'ds/no-hardcoded-colors': 'error',
+        },
+  },
+  ])
